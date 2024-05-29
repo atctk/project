bb91fd83c5612d3be871605dc897cb0e068ed7ba
atctk <lwr0404@naver.com>
2024-05-29 23:08:11 +0900

    work4

--- work1.py
+++ work1.py
@@ -1,3 +1,14 @@
-import tensorflow as tf
-if tf.test.gpu_device_name():
-    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+import tensorflow as tf 
+from tensorflow.python.client import device_lib
+
+print(device_lib.list_local_devices() )
+
+
+tensor = tf.constant([3.0,4,5])
+tensor2 = tf.constant([[1,2],[3,4]])
+
+print(tensor)
+
+w=tf.Variable(1.0)
+w.assign(2)
+print(w)

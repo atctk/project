89e52a25182a39c32123a7eda7413c2e94534814
atctk <lwr0404@naver.com>
2024-05-30 14:51:46 +0900

    work5

--- work1.py
+++ work1.py
@@ -1,14 +1,24 @@
 import tensorflow as tf 
 from tensorflow.python.client import device_lib
 
-print(device_lib.list_local_devices() )
+# print(device_lib.list_local_devices() )
+
+k=170
+s=260
+
+a = tf.Variable(0.1)
+b = tf.Variable(0.2)
+
+def lossf():
+    w = k * a + b
+    return tf.square(260 - w)
+
+opt = tf.keras.optimizers.Adam(learning_rate=0.1)
+
+for i in range(300):
+    opt.minimize(lossf,var_list=[a,b])
+    print(a.numpy(),b.numpy())
 
 
-tensor = tf.constant([3.0,4,5])
-tensor2 = tf.constant([[1,2],[3,4]])
 
-print(tensor)
 
-w=tf.Variable(1.0)
-w.assign(2)
-print(w)

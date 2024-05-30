04da9eba255f46edb223e007e302ed13a88511cd
atctk <lwr0404@naver.com>
2024-05-30 15:35:03 +0900

    work6

--- work1.py
+++ work1.py
@@ -1,24 +1,37 @@
+import pandas as pd
+
+data = pd.read_csv('gpascore.csv')
+
+# print(data.isnull().sum())
+data = data.dropna()
+
+ydata = data['admit'].values
+xdata = []
+
+for i,rows in data.iterrows():
+    xdata.append([ rows['gre'], rows['gpa'], rows['rank'] ])
+
+
+
+import numpy as np
 import tensorflow as tf 
-from tensorflow.python.client import device_lib
+# from tensorflow.python.client import device_lib
 
 # print(device_lib.list_local_devices() )
 
-k=170
-s=260
-
-a = tf.Variable(0.1)
-b = tf.Variable(0.2)
-
-def lossf():
-    w = k * a + b
-    return tf.square(260 - w)
-
-opt = tf.keras.optimizers.Adam(learning_rate=0.1)
-
-for i in range(300):
-    opt.minimize(lossf,var_list=[a,b])
-    print(a.numpy(),b.numpy())
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(64, activation='sigmoid'),
+    tf.keras.layers.Dense(128, activation='tanh'),
+    tf.keras.layers.Dense(128, activation='sigmoid'),
+    tf.keras.layers.Dense(1, activation='sigmoid'),
+])
 
 
+model.compile(optimizer='adam' , loss='binary_crossentropy' , metrics=['accuracy'])
+
+model.fit(np.array(xdata), np.array(ydata), epochs=1000)
+
+pred = model.predict( [ [750 , 3.70 ,3], [400, 2.2, 1] ] )
+print(pred)
 
 
